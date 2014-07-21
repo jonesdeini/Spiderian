e7a1e7aa652aaed0708063fd0b13c3d75413d1a9
jonesdeini <jonesdeini@gmail.com>
2014-07-21 19:10:29 -0400

    webmock FTW

--- test/the_watcher_test.rb
+++ test/the_watcher_test.rb
@@ -1,27 +1,12 @@
 require "minitest/autorun"
 require_relative '../lib/the_watcher'
-
-require 'rack'
+require 'webmock/minitest'
 
 describe TheWatcher do
 
   it "returns true when the page changes" do
-    require 'rack'
-    app = Proc.new do |env|
-          ['200', {'Content-Type' => 'text/html'}, ['bro']]
-    end
-    app2 = Proc.new do |env|
-          ['200', {'Content-Type' => 'text/html'}, ['dawg']]
-    end
-    Thread.new {
-      Rack::Handler::WEBrick.run app
-      sleep 2
-      Rack::Handler::WEBrick.shutdown
-      Rack::Handler::WEBrick.run app2
-    }
-    proc {
-      TheWatcher.new("http://localhost:8080").watch!
-    }.must_output true
+    stub_request(:get, "www.example.com").to_return({:body => "bro"}, {:body => "dawg"})
+    TheWatcher.new("http://www.example.com").watch!.must_equal true
   end
 
 end
